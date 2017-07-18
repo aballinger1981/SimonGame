00a8ed35fc2217f1e87f185ecbd9cf235a75f0e0
aballinger1981 <andrew@andrewballinger.com>
2017-07-18 16:59:08 -0400

    Created separate components for each colored button and the controls in the middle.  Started styling of buttons and centered the game on the page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TopLeftComponent } from './top-left/top-left.component';
+import { TopRightComponent } from './top-right/top-right.component';
+import { BottomLeftComponent } from './bottom-left/bottom-left.component';
+import { BottomRightComponent } from './bottom-right/bottom-right.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopLeftComponent,
+    TopRightComponent,
+    BottomLeftComponent,
+    BottomRightComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
