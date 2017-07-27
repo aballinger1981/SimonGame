5d5c60c04f87fb244b5af83de9d9fafe0ed28acf
aballinger1981 <andrew@andrewballinger.com>
2017-07-27 00:10:47 -0400

    Created startButtonState in order to start game play.

--- src/app/controls/controls.component.ts
+++ src/app/controls/controls.component.ts
@@ -7,6 +7,7 @@ import { GamePlayService } from '../game-play.service';
   styleUrls: ['./controls.component.css']
 })
 export class ControlsComponent implements OnInit {
+  public onOffState: boolean = false;
   public clickableClassToggle: string = 'unclickable';
   public strictLightToggle: string = 'strict-light-foreground-black';
   public strictMode: boolean = false;
@@ -19,17 +20,17 @@ export class ControlsComponent implements OnInit {
   }
 
   public setOnOffState(): void {
-    if (this.gamePlay.onOffState === true) {
-      this.gamePlay.onOffState = false;
+    if (this.onOffState === true) {
+      this.onOffState = false;
       this.strictLightToggle = 'strict-light-foreground-black';
     } else {
-      this.gamePlay.onOffState = true;
+      this.onOffState = true;
     }
     this.setClickabilityForStartAndStrictButtons();
   }
 
   public setClickabilityForStartAndStrictButtons(): void {
-    if (this.gamePlay.onOffState === true) {
+    if (this.onOffState === true) {
       this.clickableClassToggle = 'clickable';
     } else {
       this.clickableClassToggle = 'unclickable';
@@ -45,6 +46,14 @@ export class ControlsComponent implements OnInit {
     if (this.strictLightToggle === 'strict-light-foreground-black') {
       this.strictLightToggle = 'strict-light-foreground-red';
     } else {
+      this.strictLightToggle = 'strict-light-foreground-black';
+    }
+  }
+
+  public setStartButtonState(): void {
+    if (this.onOffState === true) {
+      this.gamePlay.computerColorSelect(this.strictMode);
+      this.clickableClassToggle = 'unclickable';
       this.strictLightToggle = 'strict-light-foreground-black';
     }
   }
--- src/app/game-play.service.ts
+++ src/app/game-play.service.ts
@@ -2,11 +2,20 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GamePlayService {
-  public onOffState: boolean = false;
   public colorPressNumber: number = 0;
   public computerColorPressMap: Map<number, string> = new Map();
+  public colorOptions: Array<string> = ['green', 'red', 'blue', 'yellow'];
+  public startButtonState: boolean = false;
 
   constructor() { }
+
+  public computerColorSelect(strictMode): void {
+    console.log(strictMode);
+
+
+    
+    this.startButtonState = true;
+  }
 
   public checkColorPress(playerColor): void {
     this.colorPressNumber++;
