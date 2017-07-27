f99bb398d80dce9ba50cadf90b7efa49f51bd0f1
aballinger1981 <andrew@andrewballinger.com>
2017-07-26 23:47:35 -0400

    Setup map to store computer color selections and function to compare user's color selection.

--- src/app/game-play.service.ts
+++ src/app/game-play.service.ts
@@ -3,9 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GamePlayService {
   public onOffState: boolean = false;
+  public colorPressNumber: number = 0;
+  public computerColorPressMap: Map<number, string> = new Map();
 
   constructor() { }
 
-  
+  public checkColorPress(playerColor): void {
+    this.colorPressNumber++;
+    const computerColor = this.computerColorPressMap.get(this.colorPressNumber);
+    if (computerColor !== playerColor) {
+
+    }
+  }
 
 }
